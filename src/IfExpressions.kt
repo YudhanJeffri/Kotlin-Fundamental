0c33f1458f4ffb1ca388376c287f6aaf9e8161c0
YudhanJeffri <yudhanjeffri168@gmail.com>
2020-08-07 10:32:53 +0700

    update ifexpressions

--- src/IfExpressions.kt
+++ src/IfExpressions.kt
@@ -4,7 +4,6 @@ fun main(){
     print("Nilai : ")
     val reader = Scanner(System.`in`)
     val nilai:Int = reader.nextInt()
-    println("Nilai Kamu : $nilai")
     if (nilai >= 75){
         println("Nilai kamu $nilai, selamat ya!")
     }else if (nilai <= 75) {
